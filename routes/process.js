b3ce34d304aaf17ea8c87a663136079cf152cec0
Elliott Foster <elliottf@codebrews.com>
2011-11-11 17:03:51 -0600

    WIP - handle async requests to the SOAP server.

--- routes/process.js
+++ routes/process.js
@@ -4,6 +4,8 @@
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/soapq');
 var requests = require('../models/models.js').requests;
+var http = requrie('http');
+var url = require('url');
 
 exports.process = function(key) {
   // Find the request we'll be processing.
@@ -14,5 +16,32 @@ exports.process = function(key) {
       return null;
     }
     console.log("Processing document: " + doc.key);
+
+    // Handle each request serially.
+    for (x in doc.payload) {
+      payload = doc.payload[x];
+      var wsdl = url.parse(payload.wsdl);
+      var options = {
+          host: wsdl.host
+        , port: wsdl.port
+        , path: wsdl.pathname
+        , method: 'POST'
+      }
+
+      if (typeof payload.security != 'undefined') {
+        options.auth = payload.security.username + ':' + payload.security.password;
+      }
+
+      // Send along the stuff!
+      console.log("Sending request to " + payload.wsdl);
+      // TODO - make this block the next request.
+      // TODO - handle errors and such.
+      http.request(options, respond(res, doc.callback));
+    }
   });
+};
+
+function respond(res, callback) {
+  // Respond to the callback with the result.
+  console.log("Sending response to " + callback);
 }
