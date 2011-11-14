951c5e26b5da90c38c79792b330ca6559b7e4d89
Elliott Foster <elliottf@codebrews.com>
2011-11-14 10:49:15 -0600

    Updating POST request.

--- routes/process.js
+++ routes/process.js
@@ -4,7 +4,7 @@
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/soapq');
 var requests = require('../models/models.js').requests;
-var http = requrie('http');
+var http = require('http');
 var url = require('url');
 
 exports.process = function(key) {
@@ -36,12 +36,21 @@ exports.process = function(key) {
       console.log("Sending request to " + payload.wsdl);
       // TODO - make this block the next request.
       // TODO - handle errors and such.
-      http.request(options, respond(res, doc.callback));
+      var req = http.request(options, function(res) { respond(res, doc); });
+      req.on('error', function(e) {
+        console.log(e);
+        console.log('There was a problem processing request with key: ' + doc.key + ' and endpoint: ' + payload.wsdl);
+      });
+      req.write(payload.envelope);
+      req.end();
     }
   });
 };
 
-function respond(res, callback) {
+function respond(res, doc) {
   // Respond to the callback with the result.
-  console.log("Sending response to " + callback);
+  console.log("Sending response to " + doc.callback);
+
+  // TODO - only delete if the shit wend off ok!
+
 }
